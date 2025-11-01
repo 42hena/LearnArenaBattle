0b748e90345712eb8c0e77d0489018ec1d338ad5
soloplay <somnium1108@gmail.com>
2025-11-02 02:09:45 +0900

    Feat: 캐릭터의 옵션을 관리할 데이터 에셋 생성
    - ABCharacterControlData 생성
    - 데이터 에셋을 통해 옵션을 편하게 관리

---
+++ Source/ArenaBattle/Character/ABCharacterControlData.cpp
@@ -0,0 +1,13 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Character/ABCharacterControlData.h"
+
+#pragma region 특수맴버함수
+
+UABCharacterControlData::UABCharacterControlData()
+{
+	TargetArmLength = 400.0f;
+}
+
+#pragma endregion
