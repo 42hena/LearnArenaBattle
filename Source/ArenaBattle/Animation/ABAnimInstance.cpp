c5172235814ce5b5ebb991b4ca38b6f9b55b4096
soloplay <somnium1108@gmail.com>
2025-11-02 14:19:21 +0900

    Feat: Idle 상태와 IdleWalkRun(BS) 구현
    - Locomotion을 캐시에 저장하는 방식 사용
    - IdleWalkRun은 Blend Space로 구현

--- Source/ArenaBattle/Animation/ABAnimInstance.cpp
+++ Source/ArenaBattle/Animation/ABAnimInstance.cpp
@@ -1,5 +1,47 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+﻿// Fill out your copyright notice in the Description page of Project Settings.
 
 
 #include "Animation/ABAnimInstance.h"
 
+#include "GameFramework/Character.h"
+#include "GameFramework/CharacterMovementComponent.h"
+
+#pragma region 특수맴버함수
+
+UABAnimInstance::UABAnimInstance()
+{
+	MovingThreshold = 3.0f;
+	JumpingThreshold = 100.0f;
+}
+
+#pragma endregion
+
+#pragma region 엔진제공함수
+
+void UABAnimInstance::NativeInitializeAnimation()
+{
+	Super::NativeInitializeAnimation();
+
+	// 굳이 필요하진 않음. 자주 쓰니까 그런거임.
+	Owner = Cast<ACharacter>(GetOwningActor());
+	if (Owner != nullptr)
+	{
+		Movement = Owner->GetCharacterMovement();
+	}
+}
+
+void UABAnimInstance::NativeUpdateAnimation(float DeltaSeconds)
+{
+	Super::NativeUpdateAnimation(DeltaSeconds);
+
+	if (Movement != nullptr)
+	{
+		Velocity = Movement->Velocity;
+		GroundSpeed = Velocity.Size2D();
+		bIsIdle = GroundSpeed < MovingThreshold;
+		bIsFalling = Movement->IsFalling();
+		bIsJumping = bIsFalling & (Velocity.Z > JumpingThreshold);
+	}
+}
+
+#pragma endregion
--- Source/ArenaBattle/Animation/ABAnimInstance.h
+++ Source/ArenaBattle/Animation/ABAnimInstance.h
@@ -1,4 +1,4 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+﻿// Fill out your copyright notice in the Description page of Project Settings.
 
 #pragma once
 
@@ -13,5 +13,48 @@ UCLASS()
 class ARENABATTLE_API UABAnimInstance : public UAnimInstance
 {
 	GENERATED_BODY()
-	
+
+#pragma region 특수맴버함수
+public:
+	UABAnimInstance();
+#pragma endregion
+
+#pragma region 엔진제공함수
+protected:
+	// 애니메이션 시스템 초기화 시 호출되는 함수
+	virtual void NativeInitializeAnimation() override;
+
+	// 매 tick마다 호출되는 함수
+	virtual void NativeUpdateAnimation(float DeltaSeconds) override;
+#pragma endregion
+
+#pragma region 맴버변수
+protected:
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Character)
+	TObjectPtr<class ACharacter> Owner;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Character)
+	TObjectPtr<class UCharacterMovementComponent> Movement;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Character)
+	FVector Velocity;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Character)
+	float GroundSpeed;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Character)
+	uint8 bIsIdle : 1;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Character)
+	float MovingThreshold;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Character)
+	uint8 bIsFalling : 1;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Character)
+	uint8 bIsJumping : 1;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Character)
+	float JumpingThreshold;
+#pragma endregion
 };
